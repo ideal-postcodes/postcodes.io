47160a0e5fcf6ef49a83742d553ab121e4e03d7f
cblanc <cablanchard@gmail.com>
2014-04-16 19:51:25 +0100

    Updated

--- config/routes.js
+++ config/routes.js
@@ -8,13 +8,13 @@ module.exports = function (app) {
 	app.get("/ping", utilsController.ping);
 	app.get("/about", pagesController.about);
 	app.get("/docs", pagesController.documentation);
+	app.get("/random/postcodes", postcodesController.random);
+	app.get("/outcodes/:outcode", postcodesController.showOutcode);	
 	app.get("/postcodes", postcodesController.query);
 	app.post("/postcodes", postcodesController.bulk);
-	app.get("/random/postcodes", postcodesController.random);
 	app.get("/postcodes/lon/:longitude/lat/:latitude", postcodesController.lonlat);
 	app.get("/postcodes/lat/:latitude/lon/:longitude", postcodesController.lonlat);
 	app.get("/postcodes/:postcode", postcodesController.show);
 	app.get("/postcodes/:postcode/validate", postcodesController.valid);
 	app.get("/postcodes/:postcode/autocomplete", postcodesController.autocomplete);	
-	app.get("/outcodes/:outcode", postcodesController.showOutcode);	
 }
