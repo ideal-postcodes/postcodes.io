2a62896359479f0ed42f3ed28b73c390a3101f02
cblanc <cablanchard@gmail.com>
2014-09-21 17:19:30 +0100

    Implemented tests for neartest postcode for postcode

--- tests/integration_postcodes.js
+++ tests/integration_postcodes.js
@@ -525,10 +525,109 @@ describe("Postcodes routes", function () {
 	});
 
 	describe("/:postcode/nearest", function () {
-		it ("returns the nearest postcodes");
-		it ("is sensitive to radius");
+
+		it ("should return a list of nearby postcodes", function (done) {
+			var uri = encodeURI("/postcodes/" + testPostcode + "/nearest");
+
+			request(app)
+			.get(uri)
+			.expect("Content-Type", /json/)
+			.expect(helper.allowsCORS)
+			.expect(200)
+			.end(function (error, response) {
+				if (error) throw error;
+				assert.isArray(response.body.result);
+				assert.isTrue(response.body.result.length > 0);
+				response.body.result.forEach(function (postcode) {
+					helper.isPostcodeObject(postcode);
+				});
+				done();
+			});
+		});
+		it ("should be sensitive to distance query", function (done) {
+			var uri = encodeURI("/postcodes/" + testPostcode + "/nearest");
+
+			request(app)
+			.get(uri)
+			.expect(200)
+			.end(function (error, firstResponse) {
+				if (error) throw error;
+				request(app)
+				.get(uri)
+				.query({
+					radius: 2000
+				})
+				.expect(200)
+				.end(function (error, secondResponse) {
+					if (error) throw error;
+					assert.isTrue(secondResponse.body.result.length >= firstResponse.body.result.length);
+					done();
+				});
+			});
+		});
+		it ("should be sensitive to limit query", function (done) {
+			var uri = encodeURI("/postcodes/" + testPostcode + "/nearest");
+
+			request(app)
+			.get(uri)
+			.query({
+				limit: 1
+			})
+			.expect(200)
+			.end(function (error, response) {
+				if (error) throw error;
+				assert.equal(response.body.result.length, 1);
+				done();
+			});
+		});
+		
+		it ("should throw a 400 error if invalid limit", function (done) {
+			var uri = encodeURI("/postcodes/" + testPostcode + "/nearest");
+
+			request(app)
+			.get(uri)
+			.query({
+				limit: "bogus"
+			})
+			.expect(400)
+			.end(function (error, response) {
+				if (error) throw error;
+				assert.equal(response.body.error, "Invalid result limit submitted");
+				done();
+			});
+		});
+
+		it ("should throw a 400 error if invalid distance", function (done) {
+			var uri = encodeURI("/postcodes/" + testPostcode + "/nearest");
+
+			request(app)
+			.get(uri)
+			.query({
+				radius: "bogus"
+			})
+			.expect(400)
+			.end(function (error, response) {
+				if (error) throw error;
+				assert.equal(response.body.error, "Invalid lookup radius submitted")
+				done();
+			});
+		});
+
+		it ("should respond to options", function (done) {
+			var uri = encodeURI("/postcodes/" + testPostcode + "/nearest");
+
+			request(app)
+			.options(uri)
+			.expect(204)
+			.end(function (error, response) {
+				if (error) done(error);
+				helper.validCorsOptions(response);
+				done();
+			});
+		});
+
 		it ("should return 404 if postcode not found", function (done) {
-			testPostcode= "ID11QE";
+			var testPostcode = "ID11QE";
 			var path = ["/postcodes/", encodeURI(testPostcode), "/nearest"].join("");
 			request(app)
 				.get(path)
@@ -540,7 +639,6 @@ describe("Postcodes routes", function () {
 					done();
 				});
 		});
-		it ("should respond to options");
 	});
 
 	describe("/random/postcode", function () {
@@ -709,8 +807,6 @@ describe("Postcodes routes", function () {
 		});
 
 		it ("should return a list of nearby postcodes", function (done) {
-			// loc = helper.locationWithNearbyPostcodes;
-			// console.log(loc);
 			var uri = encodeURI("/postcodes/lon/" + loc.longitude + "/lat/" + loc.latitude);
 
 			request(app)
