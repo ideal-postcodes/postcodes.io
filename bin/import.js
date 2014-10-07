6d597896afa34104696533c31d8d94d11305cfdb
cblanc <cablanchard@gmail.com>
2014-10-07 15:31:03 +0100

    Added warning message

--- bin/import.js
+++ bin/import.js
@@ -4,6 +4,7 @@ var	fs = require("fs"),
 		path = require("path"),
 		async = require("async"),
 		start = process.hrtime(),
+		prompt = require("prompt"),
 		sourceFile = process.argv[2],
 		env = process.env.NODE_ENV || "development",
 		Base = require(path.join(__dirname, "../app/models")),
@@ -54,17 +55,38 @@ var executionStack = [createPostgisExtension,
 											populateLocation, 
 											recreateIndexes];
 
-async.series(executionStack, function (error, result) {
+function startImport () {
+	async.series(executionStack, function (error, result) {
+		if (error) {
+			console.log("Unable to complete import process due to error", error);
+			console.log("Dropping newly created relation")
+			dropRelation(function (error, result) {
+				if (error) {
+					console.log("Unabled to drop relation");
+					process.exit(1);		
+				}		
+			});
+		}
+		console.log("Finished import process in", process.hrtime(start)[0], "seconds");
+		process.exit(0);
+	});
+}
+
+prompt.start();
+
+prompt.get([{
+	description: "Importing data will wipe your current postcode database before continuing. If you already have existing data please consider using updateons. Type YES to continue",
+  name: 'userIsSure', 
+  warning: 'Username must be only letters, spaces, or dashes'
+}], function (error, result) {
 	if (error) {
-		console.log("Unable to complete import process due to error", error);
-		console.log("Dropping newly created relation")
-		dropRelation(function (error, result) {
-			if (error) {
-				console.log("Unabled to drop relation");
-				process.exit(1);		
-			}		
-		});
+		console.log(error);
+		process.exit(1);
 	}
-	console.log("Finished import process in", process.hrtime(start)[0], "seconds");
-	process.exit(0);
+	if (result.userIsSure === "YES") {
+		startImport();
+	} else {
+		console.log("You have opted to cancel the import process");
+		process.exit(0);
+	}
 });
