7befddbd62690e6ccff16101d0c247c10bdea35a
cblanc <cablanchard@gmail.com>
2014-10-07 13:44:25 +0100

    Make acknowledgement and understanding of superuser privileges unavoidable

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,13 +6,8 @@ echo "Postcodes.io Database Setup Script"
 
 # Gather user credentials with superuser privileges
 echo "In order to create, setup and download your database we need Postgres user credentials with superuser privileges to carry out some operations.\n"
-read -p "Please provide your Postgres Username: [leave blank if your default user has superuser privileges] " POSTGRES_USER
-if [ -z "$POSTGRES_USER" ];
-then
-	PSQL="psql"
-else
-	PSQL="psql --username=$POSTGRES_USER"
-fi
+read -p "Please provide your Postgres Username: " POSTGRES_USER
+PSQL="psql --username=$POSTGRES_USER"
 
 # Create postgres user
 # With thanks to Erwin Brandstetter (http://stackoverflow.com/questions/8092086/create-postgresql-role-user-if-it-doesnt-exist)
@@ -37,25 +32,13 @@ echo "Done\n"
 sleep 1
 
 # Create Database
-
 echo "Creating new database called $DATABASE_NAME..."
-if [ -z "$POSTGRES_USER" ];
+if createdb -U $POSTGRES_USER -O $USERNAME $DATABASE_NAME
 then
-	if createdb -O $USERNAME $DATABASE_NAME
-	then
-		echo "Done\n"
-	else
-		echo "Failed to create database. Please check the error message and take action"
-		exit 1
-	fi
+	echo "Done\n"
 else
-	if createdb -U $POSTGRES_USER -O $USERNAME $DATABASE_NAME
-	then
-		echo "Done\n"
-	else
-		echo "Failed to create database. Please check the error message and take action"
-		exit 1
-	fi
+	echo "Failed to create database. Please check the error message and take action"
+	exit 1
 fi
 
 sleep 1
