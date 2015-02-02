cf04b5a1c93ad87cfe718c975d2872b43595651f
cblanc <cablanchard@gmail.com>
2015-02-02 22:15:17 +0000

    Script to prepare test db

---
+++ bin/createTestDb.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var	path = require("path");
+var helper = require(path.join(__dirname, "../tests/helper"));
+
+console.log("Wiping test database...");
+helper.connectToDb();
+helper.clearPostcodeDb(function (error, result) {
+	if (error) {
+		throw error;
+	}
+	console.log("Done.");
+	console.log("Recreating test database...");
+	helper.seedPostcodeDb(function (error, result) {
+		if (error) {
+			throw error;
+		}
+		console.log("Done.");
+		process.exit(0);
+	});
+});
