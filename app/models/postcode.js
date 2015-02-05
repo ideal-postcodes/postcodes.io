6eb829ccc3d0c675dfbe45e359ffd0a4576fab15
cblanc <cablanchard@gmail.com>
2015-02-05 22:19:48 +0000

    Finalize outcode method

--- app/models/postcode.js
+++ app/models/postcode.js
@@ -234,25 +234,48 @@ var outcodeQuery = "Select outcode, avg(northings) as northings, avg(eastings) a
 	"avg(ST_X(location::geometry)) as longitude, avg(ST_Y(location::geometry))" + 
 	" as latitude FROM postcodes WHERE outcode=$1 GROUP BY outcode";
 
-var listOutcodeAttributes = "SELECT DISTINCT $1 FROM postcodes WHERE postcodes.outcode=$2";
-var additionalAttributes = ["admin_ward"];
-
-function generateOutcodeQueries() {
-
+function generateAttrQuery (attribute) {
+	return "SELECT DISTINCT " + attribute + " FROM postcodes WHERE outcode=$1";
 }
 
+var additionalAttributes = ["admin_ward", "admin_district", "admin_county", "parish"];
+
 Postcode.prototype.findOutcode = function (outcode, callback) {
+	var self = this;
 	outcode = outcode.trim().toUpperCase();
 
 	if (!Pc.validOutcode(outcode)) {
 		return callback(null, null);
 	}
 
-	this._query(outcodeQuery, [outcode], function (error, result) {
+	self._query(outcodeQuery, [outcode], function (error, result) {
 		if (error) return callback(error, null);
-		if (result.rows === 0) return callback(null, null);
+		if (result.rows.length === 0) return callback(null, null);
 		var outcodeResult = result.rows[0];
-		return callback(null, outcodeResult);
+
+		// Start pulling in data
+		var execution = {};
+		additionalAttributes.forEach(function (attribute) {
+			execution[attribute] = function (callback) {
+				self._query(generateAttrQuery(attribute), [outcode], callback);
+			};
+		});
+
+		var handler = function (error, result) {
+			if (error) return callback(error);
+			for (var i in result) {
+				if (result.hasOwnProperty(i)) {
+					outcodeResult[i] = result[i].rows.map(function (elem) {
+						return elem[i];
+					}).filter(function (elem) {
+						return elem !== null;
+					});
+				}
+			}
+			return callback(null, outcodeResult);
+		};
+
+		async.parallel(execution, handler);
 	});
 }
 
