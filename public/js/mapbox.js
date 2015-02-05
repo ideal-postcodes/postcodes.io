2a2080880e1d80e072dae0e241018a7c00b7246f
cblanc <cablanchard@gmail.com>
2015-02-05 13:06:15 +0000

    Implemented postcode lookup and reverse geolocation

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,22 +1,56 @@
 $(function () {
 	// Remove footer
 	$("#footer").remove();
-	var startingCoordinates = [55, -2];
+	var startingCoordinates = [55.0932, -2.0472];
 	var initialZoom = 6;
-	var searchZoom = 13;
+	var searchZoom = 16;
 	$longitude = $("#longitude").html(startingCoordinates[1]);
 	$latitude = $("#latitude").html(startingCoordinates[0]);
+	$info = $("#info").hide();
+	$spinner = $("#spinner");
+	$nearestInstruction = $("#instructions");
+	$postcodeQuery = $("#postcode_query");
+	$postcodeLookup = $("#postcode_lookup");
+
+	function indicateLoading () {
+		return $spinner.show();
+	}
+
+	function indicateLoaded () {
+		setTimeout(function () {
+			return $spinner.hide();
+		}, 500);
+	}
 
 	function getPostcodesForLocation(location, success, error) {
 		var longitude = location["lng"];
 		var latitude = location["lat"];
-		$.get("/postcodes", {
+		return $.get("/postcodes", {
 			lat: latitude,
 			lon: longitude,
 			radius: 1000
 		})
 		.done(success)
 		.fail(error);
+	}
+
+	var simplePostcodeRegex = /^[a-z0-9]{1,4}\s*?\d[a-z]{2}$/i;
+
+	var queryCache = {};
+
+	function queryPostcode (postcode, success, error) {
+		var postcode = postcode.replace(/\W/, "").replace("\s", "");
+		if (queryCache[postcode]) {
+			return success(queryCache[postcode]);
+		}
+		return $.get("/postcodes", {
+				q: postcode
+			})
+			.done(function (data) {
+				queryCache[postcode] = data;
+				success(data);
+			})
+			.fail(error);
 	}
 
 	function postcodeToFeature(postcode) {
@@ -60,16 +94,66 @@ $(function () {
 
 	function handleError(data) {
 		console.log("An error occurred", data);
+		indicateLoaded();
 	}
 
 	function roundCoord(coord) {
-		return coord.toString().substring(0,6);
+		return coord.toString().substring(0,8);
+	}
+
+	var markerProperties = {
+		"postcode": "Postcode",
+		"parish": "Parish",
+		"longitude": "Longitude",
+		"latitude": "Latitude",
+		"northings": "Northings",
+		"eastings": "Eastings",
+		"admin_ward": "Ward",
+		"admin_district": "District",
+		"admin_county": "County",
+		"region": "Region",
+		"country": "Country",
+		"parliamentary_constituency": "Constituency",
+		"european_electoral_region": "European Electoral Region",
+		"ccg": "Clinical Commissioning Group"
+	};
+
+	function generateTableRow(properties) {
+		var result = [];
+		for (var prop in markerProperties) {
+			if (markerProperties.hasOwnProperty(prop) &&
+											properties[prop] !== null && 
+											properties[prop] !== "") {	
+				result.push("<tr><td>" + markerProperties[prop] + "</td><td>" + properties[prop] + "</td></tr>");
+			}
+		}
+		return result.join("");
+	}
+
+	function rebindTooltips(layers) {
+		var layerDetails = {};
+		layers.eachLayer(function (layer) {
+			var properties = layer.feature.properties;
+			layerDetails[properties["postcode"]] = generateTableRow(properties);
+		});
+
+		layers.on('mouseover', function(event) {
+			var postcode = event.layer.feature.properties["postcode"];
+			$info.html(layerDetails[postcode]).show();
+		});
+		layers.on('mouseout', function() {
+			$info.html("").hide();
+		});
 	}
 
 	// Initialise MapBox
 	L.mapbox.accessToken = 'pk.eyJ1IjoiY2FibGFuY2hhcmQiLCJhIjoibEF1SEUyNCJ9.jgoC-5g61IQobWff1f7i8A';
-	var map = L.mapbox.map('map', 'examples.map-i86nkdio')
+	var map = L.mapbox.map('map', 'examples.map-i86nkdio', {
+			zoomControl: false
+		})
 		.setView(startingCoordinates, initialZoom);
+
+	new L.Control.Zoom({ position: 'bottomleft' }).addTo(map);
 
 	var postcodesLayer = L.mapbox.featureLayer().addTo(map);
 
@@ -80,18 +164,77 @@ $(function () {
 
 	postcodesLayer.setGeoJSON(geojson);
 
+	$("#spinner").hide();
+
+	map.on("mousemove", function (event) {
+		$longitude.html(roundCoord(event.latlng["lng"]));
+		$latitude.html(roundCoord(event.latlng["lat"]));
+	});
 
 	map.on("click", function (event) {
-		console.log(event);
+		$nearestInstruction.hide();
 		var geolocation = event.latlng;
 		// Zoom exactly to each double-clicked point
 		var zoom = map.getZoom();
 		var newZoom = zoom > searchZoom ? zoom : searchZoom;
     map.setView(event.latlng, newZoom);
+    indicateLoading();
 		getPostcodesForLocation(geolocation, function (data) {
-			console.log(data);
 			var postcodes = (data.result === null) ? [] : data.result;
 			postcodesLayer.setGeoJSON(toGeoJson(postcodes));
+			rebindTooltips(postcodesLayer);
+			indicateLoaded();
 		}, handleError)
+	});
+
+	var postcodeQueries = [];
+	var DELAY = 200; // 200 ms
+
+	$postcodeQuery.keyup(function (event) {
+		// Clear request queues
+		$postcodeLookup.removeClass("has-error");
+
+		if (postcodeQueries.length > 0) {
+			postcodeQueries.forEach(function (timeout) {
+				clearTimeout(timeout);
+			});
+			postcodeQueries = [];
+		}
+
+		// Cancel request if query is too small
+		var postcode = $postcodeQuery.val();
+		if (postcode.length < 2) {
+			return;
+		} 
+
+		var timeout = setTimeout(function () {
+			var zoom = map.getZoom();
+			var newZoom = zoom > searchZoom ? zoom : searchZoom;
+			indicateLoading();
+			queryPostcode(postcode, function (data) {
+				console.log(data);
+				var postcodes = (data.result === null) ? [] : data.result;
+				if (postcodes.length > 0) {
+					var totalLat = 0; 
+					var totalLon = 0;
+					for (var i = 0; i < postcodes.length; i++) {
+						totalLat += postcodes[i].latitude;
+						totalLon += postcodes[i].longitude;
+					}
+					map.setView({
+						lat: totalLat / postcodes.length,
+						lng: totalLon / postcodes.length
+					}, newZoom);
+				}
+				if (postcodes.length === 0) {
+					$postcodeLookup.addClass("has-error");
+				}
+				// map.setView(event.latlng, newZoom);
+				postcodesLayer.setGeoJSON(toGeoJson(postcodes));
+				rebindTooltips(postcodesLayer);
+				indicateLoaded();
+			}, handleError)
+		}, DELAY);
+		postcodeQueries.push(timeout);
 	});
 });
