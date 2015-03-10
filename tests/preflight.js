3a68fce08539a746141a5a5d386cd27c9dd1c3ee
cblanc <cablanchard@gmail.com>
2015-03-10 10:07:35 +0000

    Add preflight test

---
+++ tests/preflight.js
@@ -0,0 +1,18 @@
+var path = require("path");
+var app = require(path.join(__dirname, "../server"));
+var request = require("supertest");
+var assert = require("chai").assert;
+
+describe("Preflight Requests (OPTIONS)", function () {
+	it ("should allows preflight requests", function (done) {
+		request(app)
+			.options("/")
+		  .expect(204)
+		  .end(function(error, response){
+		    if (error) return done(error);
+		    assert.equal(response.headers['access-control-allow-origin'], "*");
+		    assert.equal(response.headers['access-control-allow-methods'], "GET,POST");
+		    done();
+		  });
+	});
+});
