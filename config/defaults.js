b0915eef4d6dc21f659713a9e69dd7f8f1162a7c
Chris Blanchard <chris@iddqd.org>
2015-06-08 17:36:26 +0100

    Added env variable configuration

---
+++ config/defaults.js
@@ -0,0 +1,38 @@
+module.exports = {
+	nearest: {
+		radius: {
+			DEFAULT: 100,
+			MAX: 2000
+		},
+		limit: {
+			DEFAULT: 10,
+			MAX: 100
+		}
+	},
+	search: {
+		limit: {
+			DEFAULT: 10,
+			MAX: 100
+		}
+	},
+	bulkGeocode: {
+		geolocations: {
+			MAX: 100
+		}
+	},
+	bulkLookups: {
+		postcodes: {
+			MAX: 100
+		}
+	},
+	nearestOutcodes: {
+		radius: {
+			DEFAULT: 5000,
+			MAX: 25000
+		},
+		limit: {
+			DEFAULT: 10,
+			MAX: 100
+		}
+	}
+};
