39c66db40ed64088a03c7aeb984a386c9a02190c
Chris Blanchard <cablanchard@gmail.com>
2015-10-12 12:31:13 +0100

    Update dependencies and enable monitoring

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-var express = require("express");
-var app = express();
+"use strict";
+
 var path = require("path");
 var env = process.env.NODE_ENV || "development";
-
-// Load in config
 var config = require(path.join(__dirname, "config/config"))(env);
 
-// Load DB connection
-require(path.join(__dirname, "config/db"))(config);
+if (env === "test" || process.env.PMX) {
+	require('pmx').init({
+		http : true
+	});
+}
 
-// Start logging
+var express = require("express");
+var app = express();
+
 require(path.join(__dirname, "/config/logger"))(app, config);
-
-// Configure Express
+require(path.join(__dirname, "/config/db"))(config);
 require(path.join(__dirname, "/config/express"))(app, config);
-
-// Load in Routes
 require(path.join(__dirname, "/config/routes"))(app);
-
-// Load in Response Renderer
 require(path.join(__dirname, "/config/renderer"))(app);
 
 var port = config.port || 8000;
