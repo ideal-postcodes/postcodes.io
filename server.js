b61d33b7d3a2516d34a93055fba3501a3a8d531d
Chris Blanchard <cablanchard@gmail.com>
2016-11-18 14:22:03 +0000

    Listen for clientError

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var path = require("path");
-var env = process.env.NODE_ENV || "development";
-var config = require(path.join(__dirname, "config/config"))(env);
+const path = require("path");
+const env = process.env.NODE_ENV || "development";
+const config = require(path.join(__dirname, "config/config"))(env);
 
 if (env === "test" || process.env.PMX) {
 	require('pmx').init({
@@ -10,8 +10,8 @@ if (env === "test" || process.env.PMX) {
 	});
 }
 
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
 require(path.join(__dirname, "/config/logger"))(app, config);
 require(path.join(__dirname, "/config/db"))(config);
@@ -19,8 +19,16 @@ require(path.join(__dirname, "/config/express"))(app, config);
 require(path.join(__dirname, "/config/routes"))(app);
 require(path.join(__dirname, "/config/renderer"))(app);
 
-var port = config.port || 8000;
-app.listen(port);
+const port = config.port || 8000;
+
+const server = app.listen(port);
+
+server.on("clientError", (error, socket) => {
+	if (!socket.destroyed) {
+		socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+	}
+});
+
 console.log("Postcode API listening on port", port);
 
 exports = module.exports = app;
