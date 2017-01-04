d4cd5c010d0da31c81dd7be5f407e1b3e93ddddf
Chris Blanchard <cablanchard@gmail.com>
2017-01-04 09:44:24 +0000

    Implement show and query routes

---
+++ app/controllers/places_controller.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const logger = require("commonlog-bunyan");
+const async = require("async");
+const S = require("string");
+const Place = require("../models/place");
+const path = require("path");
+const env = process.env.NODE_ENV || "development";
+const configPath = path.join(__dirname, "../../config/config.js");
+const defaults = require(configPath)(env).defaults.placesSearch;
+
+exports.show = (request, response, next) => {
+	const id = request.params.id;
+	Place.findByCode(id.toLowerCase(), (error, place) => {
+		if (error) return next(error);
+		if (place) {
+			response.jsonApiResponse = {
+				status: 200,
+				result: Place.toJson(place)
+			};
+		} else {
+			response.jsonApiResponse = {
+				status: 404,
+				error: "Place not found"
+			};
+		}
+		next();
+	});
+}
+
+exports.query = (request, response, next) => {
+	const query = request.query.query || request.query.q;
+	if (query) {
+		searchPlace(request, response, next);
+		return;
+	}
+
+	response.jsonApiResponse = {
+		status: 400,
+		error: "No valid query submitted. Remember to include query parameter"
+	};
+
+	return next();
+};
+
+const setEmptyResponse = response => {
+	response.jsonApiResponse = {
+		status: 200,
+		result: []
+	};
+};
+
+const searchPlace = (request, response, next) => {
+	const name = request.query.query || request.query.q;
+	if (name.trim().length === 0) {
+		setEmptyResponse(response);
+		return next();
+	}
+
+	let limit = parseInt(request.query.limit, 10) 
+		|| parseInt(request.query.l, 10) 
+		|| defaults.limit.DEFAULT;
+
+	if (isNaN(limit) || limit < 1) limit = defaults.limit.DEFAULT;
+
+	Place.search({
+		name: name,
+		limit: limit
+	}, (error, places) => {
+		if (error) return next(error);
+		if (!places) {
+			setEmptyResponse(response);
+			return next();
+		}
+		response.jsonApiResponse = {
+			status: 200,
+			result: places.map(p => Place.toJson(p))
+		}
+		return next();
+	});
+};
--- config/routes.js
+++ config/routes.js
@@ -1,26 +1,36 @@
-var path = require("path");
-var pagesController = require(path.join(__dirname, "../app/controllers/pages_controller"));
-var postcodesController = require(path.join(__dirname, "../app/controllers/postcodes_controller"));
-var outcodesController = require(path.join(__dirname, "../app/controllers/outcodes_controller"));
-var utilsController = require(path.join(__dirname, "../app/controllers/utils_controller"));
+"use strict";
 
-module.exports = function (app) {
-	app.get("/", pagesController.home);
-	app.get("/ping", utilsController.ping);
-	app.get("/about", pagesController.about);
-	app.get("/docs", pagesController.documentation);
-	app.get("/explore", pagesController.explore);
-	app.get("/random/postcodes", postcodesController.random);
-	app.get("/postcodes", postcodesController.query);
-	app.post("/postcodes", postcodesController.bulk);
-	app.get("/postcodes/lon/:longitude/lat/:latitude", postcodesController.lonlat);
-	app.get("/postcodes/lat/:latitude/lon/:longitude", postcodesController.lonlat);
-	app.get("/postcodes/:postcode", postcodesController.show);
-	app.get("/postcodes/:postcode/nearest", postcodesController.nearest);
-	app.get("/postcodes/:postcode/validate", postcodesController.valid);
-	app.get("/postcodes/:postcode/autocomplete", postcodesController.autocomplete);	
+const path = require("path");
+const controllerPath = path.join(__dirname, "../app/controllers");
+const pages = require(path.join(controllerPath, "/pages_controller"));
+const utils = require(path.join(controllerPath, "/utils_controller"));
+const places = require(path.join(controllerPath, "/places_controller"));
+const outcodes = require(path.join(controllerPath, "/outcodes_controller"));
+const postcodes = require(path.join(controllerPath, "/postcodes_controller"));
 
-	app.get("/outcodes", outcodesController.query);
-	app.get("/outcodes/:outcode", outcodesController.showOutcode);
-	app.get("/outcodes/:outcode/nearest", outcodesController.nearest);
+module.exports = app => {
+	app.get("/", pages.home);
+	app.get("/ping", utils.ping);
+	app.get("/about", pages.about);
+	app.get("/docs", pages.documentation);
+	app.get("/explore", pages.explore);
+	
+	app.get("/postcodes", postcodes.query);
+	app.post("/postcodes", postcodes.bulk);
+	app.get("/random/postcodes", postcodes.random);
+	app.get("/postcodes/:postcode", postcodes.show);
+	app.get("/postcodes/:postcode/nearest", postcodes.nearest);
+	app.get("/postcodes/:postcode/validate", postcodes.valid);
+	app.get("/postcodes/:postcode/autocomplete", postcodes.autocomplete);	
+	app.get("/postcodes/lon/:longitude/lat/:latitude", postcodes.lonlat);
+	app.get("/postcodes/lat/:latitude/lon/:longitude", postcodes.lonlat);
+
+	app.get("/outcodes", outcodes.query);
+	app.get("/outcodes/:outcode", outcodes.showOutcode);
+	app.get("/outcodes/:outcode/nearest", outcodes.nearest);
+
+	app.get("/places/:id", places.show);
+	app.get("/places", places.query);
+	// Todo: Query for geolocation contained in polygon (geolocation contained in polygon)
+	// Todo: Query for geolocation near polygon (radius intersects polygon)
 };
