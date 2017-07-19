3fd6b31f9f4dca3b249d6875d9e80a1330026163
Christopher Blanchard <cablanchard@gmail.com>
2017-07-19 09:49:53 +0100

    Lint

--- config/filter.js
+++ config/filter.js
@@ -1,17 +1,18 @@
 "use strict";
+
 const isObject = o => Object.prototype.toString.call(o) === "[object Object]";
 const isArray = o => Object.prototype.toString.call(o) === "[object Array]";
 
-const objFilter = (obj, filterArray) => { //fuilters an object with valid filters in the filterArray returns an Object
+const objFilter = (obj, filterArray) => { //filters an object with valid filters in the filterArray returns an Object
   return filterArray.reduce((acc,curr) => {
    if (obj[curr]) acc[curr] = obj[curr];
    return acc;
  } , {});
-}
+};
 
 const arrFilter = (arr, filterArray) => { //filters an array of objects with objFilter returns an Array
   return arr.map(obj => objFilter(obj,filterArray));
-}
+};
 
 const filter = (jsonResultProp, filterArray) => {
   if (jsonResultProp === null) return null;
@@ -25,14 +26,14 @@ const filter = (jsonResultProp, filterArray) => {
 const requiresFilter = (request, response) => {
   const jsonResponse = response.jsonApiResponse;
   return (jsonResponse && request.query.filter && jsonResponse.status === 200);
-}
+};
 
 const filterArray = request => {
   return request.query.filter.replace(/\s/g, "")
                              .toLowerCase()
                              .split(",");
-}
-//=========================================================
+};
+
 const filterMiddleware = (request, response, next) => {
 
   if (!requiresFilter(request, response)) return next();
@@ -43,12 +44,10 @@ const filterMiddleware = (request, response, next) => {
         
   resultData.forEach(obj => {
     obj.result = filter(obj.result, filteredArray); //obj.result either an object or an array
-  })
+  });
   
   return next();
   
-}
-
-//============================================================
+};
 
 module.exports = filterMiddleware;
