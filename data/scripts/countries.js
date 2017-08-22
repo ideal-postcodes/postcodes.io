628ede53e13d3e8c6d6cf35edd937d9179929411
Chris Blanchard <cablanchard@gmail.com>
2017-08-22 15:18:32 +0100

    Document and regenerate countries.json

---
+++ data/scripts/countries.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const {extract, toJson} = require("./index");
+
+/**
+ * @module DataParser/countries
+ *
+ * Writes countries.json to stdout
+ * 
+ * Notes:
+ * - Raw TSV data is malformed and has inconsistent column counts requiring
+ * `relax_column_count: true`
+ */
+
+const transform = row => {
+	const keyIndex = 0;
+	const valueIndex = 3;
+	if (row[keyIndex] === "CTRY12CD") return []; // Escape if header
+	return [row[keyIndex], row[valueIndex]];
+};
+
+const parseOptions = { relax_column_count: true };
+
+const configs = [
+	{
+		file: "Country names and codes UK as at 08_12.txt",
+		transform,
+		parseOptions
+	}
+];
+
+extract({
+	configs,
+	done: (error, result) => {
+		if (error) {
+			console.log(error);
+			process.exit(1);
+		}
+		console.log(toJson(result));
+		process.exit(0);
+	}
+});
