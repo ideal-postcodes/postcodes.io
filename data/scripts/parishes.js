b73e0e1ac48972d7ec827cc690df9771d3ffd6c7
Chris Blanchard <cablanchard@gmail.com>
2017-09-06 15:38:09 +0100

    Add unparished codes to parish parser

--- data/scripts/parishes.js
+++ data/scripts/parishes.js
@@ -18,10 +18,24 @@ const transform = row => {
 	return [code, value];
 };
 
+const UNPARISHED_CODE_OFFSET = 0;
+const UNPARISHED_VALUE_OFFSET = 1;
+
+const unparishedTransform = row => {
+	const code = row[UNPARISHED_CODE_OFFSET];
+	const value = row[UNPARISHED_VALUE_OFFSET];
+	if (code === "NCP16CD") return []; // Escape if header
+	return [code, value];
+};
+
 const configs = [
 	{
 		file: "Parish LAD names and codes UK as at 12_15.txt",
 		transform
+	},
+	{
+		file: "Unparished areas names and codes EN as at 12_16.txt",
+		transform: unparishedTransform
 	}
 ];
 
