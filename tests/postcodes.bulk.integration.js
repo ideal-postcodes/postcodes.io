def09d094910a4f4837e51c916a227715398e09c
Chris Blanchard <cablanchard@gmail.com>
2017-09-12 12:14:07 +0100

    Refactor error renderer, capture invalid JSON input, add tests

--- tests/postcodes.bulk.integration.js
+++ tests/postcodes.bulk.integration.js
@@ -1,74 +1,88 @@
-var path = require("path"),
-		app = require(path.join(__dirname, "../server")),
-		request = require("supertest"),
-		assert = require("chai").assert,
-		helper = require(__dirname + "/helper")
-		async = require("async");
+"use strict";
 
-describe("Postcodes routes", function () {
-	var testPostcode;
+const app = require("../server");
+const request = require("supertest");
+const assert = require("chai").assert;
+const helper = require("./helper");
+const async = require("async");
+
+describe("Postcodes routes", () => {
+	let testPostcode, testOutcode;
 
 	before(function (done) {
 		this.timeout(0);
-		helper.clearPostcodeDb(function (error, result) {
+		helper.clearPostcodeDb((error, result) => {
 			if (error) return done(error);
-			helper.seedPostcodeDb(function (error, result) {
+			helper.seedPostcodeDb((error, result) => {
 				if (error) return done(error);
 				done();
 			});
 		});
 	});
 
-	beforeEach(function (done) {
-		helper.lookupRandomPostcode(function (result) {
+	beforeEach(done => {
+		helper.lookupRandomPostcode(result => {
 			testPostcode = result.postcode;
 			testOutcode = result.outcode;
 			done();
 		});
 	});
 
-	after(function (done) {
-		helper.clearPostcodeDb(done);
-	});
+	after(helper.clearPostcodeDb);
 
-	describe("POST /postcodes", function () {
-		var bulkLength = 10,
-				testPostcodes, testLocations;
+	describe("POST /postcodes", () => {
+		let bulkLength = 10;
+		let testPostcodes, testLocations;
 
-		describe("Bulk geocoding", function () {
-			beforeEach(function (done) {
-				async.times(bulkLength, function (n, next) {
+		describe("Invalid JSON submission", () => {
+			it ("returns 400 on invalid JSON", done => {
+				request(app)
+				.post("/postcodes")
+				.set({"Content-Type": "application/json"})
+				.send("}{")
+				.expect("Content-Type", /json/)
+				.expect(helper.allowsCORS)
+				.expect(400)
+				.end((error, response) => {
+					if (error) return done(error);
+					assert.match(response.body.error, /invalid json submitted/ig);
+					done();
+				});
+			});
+		});
+
+		describe("Bulk geocoding", () => {
+			beforeEach(done => {
+				async.times(bulkLength, (n, next) => {
 					helper.randomLocation(next);
-				}, function (error, locations) {
+				}, (error, locations) => {
 					if (error) return done(error);
 					testLocations = locations;
 					done();
 				});
 			});
 
-			it ("should return postcodes for specified geolocations", function (done) {
+			it ("should return postcodes for specified geolocations", done => {
 				request(app)
 				.post("/postcodes")
 				.send({geolocations: testLocations})
 				.expect("Content-Type", /json/)
 				.expect(helper.allowsCORS)
 				.expect(200)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					assert.isArray(response.body.result);
 					assert.equal(response.body.result.length, bulkLength);
-					response.body.result.forEach(function (lookup) {
+					response.body.result.forEach(lookup => {
 						assert.property(lookup, "query");
 						assert.property(lookup, "result");
 						assert.isArray(lookup.result);
-						lookup.result.forEach(function (result) {
-							helper.isPostcodeWithDistanceObject(result);
-						});
+						lookup.result.forEach(result => helper.isPostcodeObject(result));
 					});
 					done();
 				});
 			});
-			it ("should return null if no nearby postcode", function (done) {
+			it ("should return null if no nearby postcode", done => {
 				request(app)
 				.post("/postcodes")
 				.send({geolocations: [{
@@ -78,16 +92,16 @@ describe("Postcodes routes", function () {
 				.expect("Content-Type", /json/)
 				.expect(helper.allowsCORS)
 				.expect(200)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					assert.equal(response.body.result.length, 1);
 					assert.isNull(response.body.result[0].result);
 					done();
 				});
 			});
-			it ("should refuse request if lookups number over 100", function (done) {
+			it ("should refuse request if lookups number over 100", done => {
 				testLocations = [];
-				for (var i = 0; i < 101; i++) {
+				for (let i = 0; i < 101; i++) {
 					testLocations.push("bogus")
 				}
 				request(app)
@@ -96,27 +110,27 @@ describe("Postcodes routes", function () {
 				.expect("Content-Type", /json/)
 				.expect(helper.allowsCORS)
 				.expect(400)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					assert.match(response.body.error, /too many locations submitted/i);
 					done();
 				});
 			});
-			it ("should return 404 if invalid geolocations object", function (done) {
+			it ("should return 404 if invalid geolocations object", done => {
 				request(app)
 				.post("/postcodes")
 				.send({geolocations: "Bogus"})
 				.expect("Content-Type", /json/)
 				.expect(helper.allowsCORS)
 				.expect(400)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					assert.match(response.body.error, /Invalid data submitted/i);
 					done();
 				});
 			});
-			it ("is sensitive to limit", function (done) {
-				var testLocation = testLocations[0];
+			it ("is sensitive to limit", done => {
+				const testLocation = testLocations[0];
 				testLocation.limit = 1;
 				request(app)
 				.post("/postcodes")
@@ -124,7 +138,7 @@ describe("Postcodes routes", function () {
 				.expect("Content-Type", /json/)
 				.expect(helper.allowsCORS)
 				.expect(200)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					assert.equal(response.body.result.length, 1);
 					assert.equal(response.body.result[0].result.length, 1);
@@ -132,8 +146,8 @@ describe("Postcodes routes", function () {
 					done();
 				});
 			});
-			it ("is sensitive to radius", function (done) {
-				var testLocation = testLocations[0];
+			it ("is sensitive to radius", done => {
+				const testLocation = testLocations[0];
 				testLocation.limit = 100;
 				testLocation.radius = 100;
 				request(app)
@@ -142,11 +156,11 @@ describe("Postcodes routes", function () {
 				.expect("Content-Type", /json/)
 				.expect(helper.allowsCORS)
 				.expect(200)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					assert.isTrue(response.body.result[0].result.length > 0);
-					var count = response.body.result[0].result.length;
-					var query = response.body.result[0].query;
+					const count = response.body.result[0].result.length;
+					const query = response.body.result[0].query;
 					assert.isDefined(query.limit);
 					assert.equal(query.radius, testLocation.radius);
 					helper.isPostcodeWithDistanceObject(response.body.result[0].result[0]);
@@ -155,26 +169,26 @@ describe("Postcodes routes", function () {
 						.post("/postcodes")
 						.send({geolocations: [testLocation]})
 						.expect(200)
-						.end(function (error, response) {
+						.end((error, response) => {
 							if (error) return done(error);
 							assert.isTrue(response.body.result[0].result.length > count);
 							done();
 						});
 				});
 			});
-			it ("allows wide area searches", function (done) {
-				var testLocation = {
+			it ("allows wide area searches", done => {
+				const testLocation = {
 					longitude: -2.12659411941741,
 					latitude: 57.2465923827836,
 					wideSearch: true
-				}
+				};
 				request(app)
 				.post("/postcodes")
 				.send({geolocations: [testLocation]})
 				.expect("Content-Type", /json/)
 				.expect(helper.allowsCORS)
 				.expect(200)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					assert.equal(response.body.result[0].result.length, 10);
 					assert.isTrue(response.body.result[0].query["wideSearch"]);
@@ -183,19 +197,19 @@ describe("Postcodes routes", function () {
 					done();
 				});
 			});
-			it ("allows wide area searches using 'widesearch'", function (done) {
-				var testLocation = {
+			it ("allows wide area searches using 'widesearch'", done => {
+				const testLocation = {
 					longitude: -2.12659411941741,
 					latitude: 57.2465923827836,
 					widesearch: true
-				}
+				};
 				request(app)
 				.post("/postcodes")
 				.send({geolocations: [testLocation]})
 				.expect("Content-Type", /json/)
 				.expect(helper.allowsCORS)
 				.expect(200)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					assert.equal(response.body.result[0].result.length, 10);
 					assert.isTrue(response.body.result[0].query["widesearch"]);
@@ -206,28 +220,28 @@ describe("Postcodes routes", function () {
 			});
 		});
 
-		describe("Bulk postcode lookup", function () {
-			beforeEach(function (done) {
-				async.times(bulkLength, function (n, next) {
+		describe("Bulk postcode lookup", () => {
+			beforeEach(done => {
+				async.times(bulkLength, (n, next) => {
 					helper.randomPostcode(next);
-				}, function (error, postcodes) {
+				}, (error, postcodes) => {
 					if (error) return done(error);
 					testPostcodes = postcodes;
 					done();
 				});
 			});
 
-			it ("should return addresses for postcodes", function (done) {
+			it ("should return addresses for postcodes", done => {
 				request(app)
 				.post("/postcodes")
 				.send({postcodes: testPostcodes})
 				.expect('Content-Type', /json/)
 				.expect(200)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					assert.isArray(response.body.result);
 					assert.equal(response.body.result.length, bulkLength);
-					response.body.result.forEach(function (lookup) {
+					response.body.result.forEach(lookup => {
 						assert.property(lookup, "query");
 						assert.property(lookup, "result");
 						helper.isPostcodeObject(lookup.result);
@@ -235,13 +249,13 @@ describe("Postcodes routes", function () {
 					done();
 				});
 			});
-			it ("should return an empty result for non string queries", function (done) {
+			it ("should return an empty result for non string queries", done => {
 				request(app)
 				.post("/postcodes")
 				.send({postcodes: [null]})
 				.expect('Content-Type', /json/)
 				.expect(200)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					assert.isArray(response.body.result);
 					assert.equal(response.body.result.length, 1);
@@ -250,26 +264,24 @@ describe("Postcodes routes", function () {
 					done();
 				});
 			});
-			it ("should return a null if postcode not found", function (done) {
+			it ("should return a null if postcode not found", done => {
 				testPostcodes.push("B0GUS");
 				request(app)
 				.post("/postcodes")
 				.send({postcodes: testPostcodes})
 				.expect('Content-Type', /json/)
 				.expect(200)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					assert.equal(response.body.result.length, bulkLength + 1);
-					var hasNull = response.body.result.some(function (lookup) {
-						return lookup.result === null;
-					});
+					let hasNull = response.body.result.some(l => l.result === null);
 					assert.isTrue(hasNull);
 					done();
 				});
 			});
-			it ("should refuse requests if lookups number over 100", function (done) {
+			it ("should refuse requests if lookups number over 100", done => {
 				testPostcodes = [];
-				for (var i = 0; i < 101; i++) {
+				for (let i = 0; i < 101; i++) {
 					testPostcodes.push("bogus");
 				}
 				request(app)
@@ -277,20 +289,20 @@ describe("Postcodes routes", function () {
 				.send(testPostcodes)
 				.expect('Content-Type', /json/)
 				.expect(400)
-				.end(function (error, response) {
+				.end((error, response) => {
 					if (error) return done(error);
 					done();
 				});
 			});
 		});
-
-		it ("should return a 400 error if array not submitted", function (done) {
+		
+		it ("should return a 400 error if array not submitted", done => {
 			request(app)
 			.post("/postcodes")
 			.send({"wrong" : "dataType"})
 			.expect('Content-Type', /json/)
 			.expect(400)
-			.end(function (error, response) {
+			.end((error, response) => {
 				if (error) return done(error);
 				assert.match(response.body.error, /ensure that Content-Type is set to application\/json/);
 				done();
