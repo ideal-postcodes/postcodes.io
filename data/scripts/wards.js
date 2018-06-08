f152744fb74a6308055f95fb5e892f32baa8efd6
Chris Blanchard <cablanchard@gmail.com>
2018-06-08 14:47:14 +0100

    Update wards

--- data/scripts/wards.js
+++ data/scripts/wards.js
@@ -14,14 +14,18 @@ const VALUE_OFFSET = 1;
 const transform = row => {
 	const code = row[CODE_OFFSET];
 	const value = row[VALUE_OFFSET];
-	if (code === "WD17CD") return []; // Escape if header
+	if (code === "WD18CD") return []; // Escape if header
 	return [code, value];
 };
 
 const configs = [
 	{
-		file: "Ward names and codes UK as at 12_17.txt",
-		transform
+		file: "Ward names and codes UK as at 05_18.csv",
+		transform,
+		parseOptions: {
+			delimiter: ",",
+		},
+		encoding: "utf8",
 	}
 ];
 
