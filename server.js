da0af6773f05d808cbf7f8d5b6a9d94bff44885e
Chris Blanchard <cablanchard@gmail.com>
2018-10-26 19:00:15 +0200

    Add graceful exit from SIGTERM (docker)

--- server.js
+++ server.js
@@ -2,26 +2,26 @@
 
 const path = require("path");
 const env = process.env.NODE_ENV || "development";
-const config = require(path.join(__dirname, "config/config"))(env);
-
-if (env === "test" || process.env.PMX) {
-	require("pmx").init({
-		http : true
-	});
-}
-
-const app = require(path.join(__dirname, "app.js"))(config);
-
+const config = require("./config/config")(env);
+const app = require("./app.js")(config);
+const { logger } = require("commonlog-bunyan");
 const port = config.port || 8000;
 
-const server = app.listen(port);
+if (env === "test" || process.env.PMX) require("pmx").init({ http : true });
 
+const server = app.listen(port);
 server.on("clientError", (error, socket) => {
 	if (!socket.destroyed) {
 		socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
 	}
 });
 
-console.log("Postcode API listening on port", port);
+process.on("SIGTERM", () => {
+  logger.info("Quitting Postcode API");
+  process.exit(0);
+});
+
+logger.info("Postcode API listening on port", port);
 
 module.exports = app;
+
