582980ddb6cb7a34cbf85118613645b751b27a4f
Chris Blanchard <cablanchard@gmail.com>
2018-10-26 17:03:53 +0200

    Drop path from app

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 "use strict";
 
-const path = require("path");
 const express = require("express");
 
 exports = module.exports = config => {
 	const app = express();
-	require(path.join(__dirname, "/config/logger"))(app, config);
-	require(path.join(__dirname, "/config/db"))(config);
-	require(path.join(__dirname, "/config/express"))(app, config);
-	require(path.join(__dirname, "/config/routes"))(app);
-	require(path.join(__dirname, "/config/renderer"))(app);
+	require("./config/logger")(app, config);
+	require("./config/db")(config);
+	require("./config/express")(app, config);
+	require("./config/routes")(app);
+	require("./config/renderer")(app);
 	return app;
 };
