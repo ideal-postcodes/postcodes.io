c7dc70816aba40bd5ea2c78e28d4ca42cd75a97d
Chris Blanchard <cablanchard@gmail.com>
2018-12-03 12:02:23 +0100

    Move out pg helper methods

--- tests/helper/index.js
+++ tests/helper/index.js
@@ -377,60 +377,6 @@ function testOutcode(o) {
 	].forEach(prop => assert.property(o, prop));
 }
 
-// Credit: https://www.peterbe.com/plog/select-all-relations-in-postgresql
-const databaseRelationsQuery = `
-	SELECT
-		c.relname as "Name",
-		CASE c.relkind WHEN 'r' THEN 'table'
-		WHEN 'v' THEN 'view'
-		WHEN 'm' THEN 'materialized view'
-		WHEN 'i' THEN 'index'
-		WHEN 'S' THEN 'sequence'
-		WHEN 's' THEN 'special'
-		WHEN 'f' THEN 'foreign table' END as "Type"
-	FROM pg_catalog.pg_class c
-			 LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-	WHERE c.relkind IN ('r','v','m','S','f','')
-				AND n.nspname <> 'pg_catalog'
-				AND n.nspname <> 'information_schema'
-				AND n.nspname !~ '^pg_toast'
-		AND pg_catalog.pg_table_is_visible(c.oid);
-`;
-
-// List relations in database
-function listDatabaseRelations(cb) {
-	Base.prototype._query(databaseRelationsQuery, cb);
-}
-
-// Credit: https://stackoverflow.com/questions/6777456/list-all-index-names-column-names-and-its-table-name-of-a-postgresql-database
-const databaseIndexesQuery = `
-	SELECT i.relname as indname,
-	       i.relowner as indowner,
-	       idx.indrelid::regclass,
-	       am.amname as indam,
-	       idx.indkey,
-	       ARRAY(
-	       SELECT pg_get_indexdef(idx.indexrelid, k + 1, true)
-	       FROM generate_subscripts(idx.indkey, 1) as k
-	       ORDER BY k
-	       ) as indkey_names,
-	       idx.indexprs IS NOT NULL as indexprs,
-	       idx.indpred IS NOT NULL as indpred
-	FROM   pg_index as idx
-	JOIN   pg_class as i
-	ON     i.oid = idx.indexrelid
-	JOIN   pg_am as am
-	ON     i.relam = am.oid
-	JOIN   pg_namespace as ns
-	ON     ns.oid = i.relnamespace
-	AND    ns.nspname = ANY(current_schemas(false));
-`;
-
-// Lists indexes in database
-function listDatabaseIndexes(cb) {
-	Base.prototype._query(databaseIndexesQuery, cb);
-}
-
 module.exports = {
 	// Data
 	config,
@@ -462,9 +408,10 @@ module.exports = {
 	isRawPostcodeObjectWithFC,
 	isRawPostcodeObjectWithFCandDistance,
 	lookupRandomPostcode,
-	listDatabaseRelations,
-	listDatabaseIndexes,
 	locationWithNearbyPostcodes,
+
+  // PG helper methods
+  ...require("./pg.js"),
 
   // Test suites
   AttributeBaseSuite,
