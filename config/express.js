3bcd4ba9c7737326c6d6a4672b86928742325e18
Christopher Blanchard <cablanchard@gmail.com>
2019-02-20 14:19:13 +0000

    Deprecate underutilisted config variables:
    - `env`
    - `root`

--- config/express.js
+++ config/express.js
@@ -1,25 +1,26 @@
 "use strict";
 
+const env = process.env.NODE_ENV || "development";
 const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
 const express = require("express");
-const path = require("path");
+const { join } = require("path");
 const cors = require("cors")({
 	origin: "*",
 	methods: "GET,POST,OPTIONS",
 	allowedHeaders: "X-Requested-With, Content-Type, Accept, Origin"
 });
 
-module.exports = (app, config) => {
+module.exports = app => {
 	app.enable("trust proxy");
 	app.disable("x-powered-by");
-	app.set("views", path.join(config.root, "app/views"));
+  app.set("views", join(__dirname, "../app/views"));
   app.set("view engine", "ejs");
 
 	// Should be handled by webserver in production
-	if (config.env !== "production") {
+	if (env !== "production") {
 		app.use(favicon("public/favicon.ico"));
-		app.use(express.static(path.join(config.root, "/public")));
+    app.use(express.static(join(__dirname, "../public")));
 	}
 	app.use(cors);
 	app.use(bodyParser.json());
