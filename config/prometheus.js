14255d3c1680135c8298542a1a253cd8679a79e6
Christopher Blanchard <cablanchard@gmail.com>
2019-03-01 14:59:53 +0000

    Implement path normalisation for prometheus endpoint tracking

--- config/prometheus.js
+++ config/prometheus.js
@@ -13,6 +13,26 @@ const promClient = {
 };
 
 /**
+ * Squash metrics with dynamic paths into one
+ *
+ * e.g. /postcodes/sw1a2aa -> /postcodes/:postcode
+ */
+const normalizePath = [
+  [
+    `^/postcodes/(lat|lon)/\\d+(\\.\\d+)?/(lat|lon)/\\d+(\\.\\d+)?$`,
+    "/postcodes/lon/:lon/lat/:lat",
+  ],
+  ["^/postcodes/[^/]+$", "/postcodes/:postcode"],
+  ["^/postcodes/.+/validate$", "/postcodes/:postcode/validate"],
+  ["^/postcodes/.+/nearest", "/postcodes/:postcode/nearest"],
+  ["^/postcodes/.+/autocomplete", "/postcodes/:postcode/autocomplete"],
+  ["^/outcodes/[^/]+$", "/outcodes/:outcode"],
+  ["^/outcodes/.+/nearest$", "/outcodes/:outcode/nearest"],
+  ["^/terminated_postcodes/[^/]+$", "/terminated_postcodes/:postcode"],
+  ["^/places/[^/]+$", "/places/:code"],
+];
+
+/**
  * Inserts optional prometheus monitoring middleware
  *
  * express-prom-bundle exposes a '/metrics' endpoint which can be queried by prometheus
@@ -28,11 +48,11 @@ module.exports = (app, { prometheusUsername, prometheusPassword }) => {
   // Apply basic auth middleware
   const users = {};
   users[prometheusUsername] = prometheusPassword;
-
   app.use(metricsPath, auth({ users, challenge: true }));
 
   // Apply prometheus middleware
   const prometheusMiddleware = prometheus({
+    normalizePath,
     metricsPath,
     buckets,
     includeMethod: true,
