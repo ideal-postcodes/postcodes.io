155d060ea71d7f30d11bf60453242ef25f8d1667
Christopher Blanchard <cablanchard@gmail.com>
2019-03-01 10:23:03 +0000

    Add prometheus metrics capability behind config flag

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 "use strict";
 
 const express = require("express");
+const prometheus = require("express-prom-bundle");
 
 exports = module.exports = config => {
-	const app = express();
-	require("./config/logger")(app, config);
-	require("./config/db")(config);
-	require("./config/express")(app, config);
-	require("./config/routes")(app);
-	require("./config/renderer")(app);
-	return app;
+  const app = express();
+  require("./config/logger")(app, config);
+  require("./config/db")(config);
+  require("./config/express")(app, config);
+  require("./config/prometheus")(app, config);
+  require("./config/routes")(app);
+  require("./config/renderer")(app);
+  return app;
 };
