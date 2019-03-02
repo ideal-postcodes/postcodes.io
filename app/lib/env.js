d33ac0727156451f537aaea80dbdf19cf517fe5a
Christopher Blanchard <cablanchard@gmail.com>
2019-03-02 15:07:46 +0000

    Add test for null

--- app/lib/env.js
+++ app/lib/env.js
@@ -6,15 +6,17 @@
  * Parses environemnt variable value (`process.env["KEY"]`) returning appropriate type
  *
  * @param VAR {string|undefined}
- * @param defaultValue {string|number|boolean|undefined}
- * @returns {string|number|boolean|undefined}
+ * @param defaultValue {string|number|boolean|undefined|null}
+ * @returns {string|number|boolean|undefined|null}
  */
 const parseEnv = (VAR, defaultValue) => {
   if (VAR === undefined) return defaultValue;
   const n = parseInt(VAR, 10);
   if (!isNaN(n)) return n;
-  if (VAR.toLowerCase() === "true") return true;
-  if (VAR.toLowerCase() === "false") return false;
+  const lcVar = VAR.toLowerCase();
+  if (lcVar === "true") return true;
+  if (lcVar === "false") return false;
+  if (lcVar === "null") return null;
   return VAR;
 };
 
