987063257d6230aeb81e98514990b3b78706f2a0
Christopher Blanchard <cablanchard@gmail.com>
2019-03-03 08:56:12 +0000

    Rebuild NHS HA codes

--- data/scripts/nhsHa.js
+++ data/scripts/nhsHa.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const {extract} = require("./index");
+const { extract } = require("./index");
 
 /**
  * @module DataParser/nhsHa
@@ -16,20 +16,20 @@ const CODE_OFFSET = 0;
 const VALUE_OFFSET = 2;
 
 const transform = row => {
-	const code = row[CODE_OFFSET];
-	const value = row[VALUE_OFFSET];
-	if (code === "HLTHAUCD") return []; // Escape if header
-	return [code, value];
+  const code = row[CODE_OFFSET];
+  const value = row[VALUE_OFFSET];
+  if (value === "HLTHAUNM") return []; // Escape if header
+  return [code, value];
 };
 
-const parseOptions = { relax_column_count: true };
+const parseOptions = { delimiter: "," };
 
 const configs = [
-	{
-		file: "HLTHAU names and codes UK as at 12_16 (OSHLTHAU).txt",
-		transform,
-		parseOptions
-	}
+  {
+    file: "HLTHAU names and codes UK as at 12_18 (OSHLTHAU).csv",
+    transform,
+    parseOptions,
+  },
 ];
 
 extract({ configs });
