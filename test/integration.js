4b106315bd921f8b1339550fea86b37708410559
Christopher Blanchard <cablanchard@gmail.com>
2019-05-22 10:34:28 +0200

    chore(Prettier): Apply fixer

--- test/integration.js
+++ test/integration.js
@@ -1,84 +1,75 @@
 "use strict";
 
-const app = require("../server");
+const { postcodesioApplication } = require("./helper");
 const request = require("supertest");
-const assert = require("chai").assert;
+const { assert } = require("chai");
+const app = postcodesioApplication();
 
 describe("Pages routes", () => {
-	describe("/", () => {
-		it ("should return 200", done => {
-			request(app)
-				.get("/")
-				.expect('Content-Type', /html/)
-				.expect(200)
-				.end(done);
-		});
-	});
+  describe("/", async () => {
+    it("should return 200", async () => {
+      await request(app)
+        .get("/")
+        .expect("Content-Type", /html/)
+        .expect(200);
+    });
+  });
 
-	describe("/docs", () => {
-		it ("should return 200", done => {
-			request(app)
-				.get("/docs")
-				.expect('Content-Type', /html/)
-				.expect(200)
-				.end(done);
-		});
-	});
+  describe("/docs", () => {
+    it("should return 200", async () => {
+      await request(app)
+        .get("/docs")
+        .expect("Content-Type", /html/)
+        .expect(200);
+    });
+  });
 
-	describe("/about", () => {
-		it ("should return 200", done => {
-			request(app)
-				.get("/about")
-				.expect("Content-Type", /html/)
-				.expect(200)
-				.end(done);
-		});
-	});
+  describe("/about", () => {
+    it("should return 200", async () => {
+      await request(app)
+        .get("/about")
+        .expect("Content-Type", /html/)
+        .expect(200);
+    });
+  });
 
-	describe("/explore", () => {
-		it ("should return 200", done => {
-			request(app)
-				.get("/explore")
-				.expect("Content-Type", /html/)
-				.expect(200)
-				.end(done);
-		});
-	});
+  describe("/explore", () => {
+    it("should return 200", async () => {
+      await request(app)
+        .get("/explore")
+        .expect("Content-Type", /html/)
+        .expect(200);
+    });
+  });
 });
 
 describe("Errors", () => {
-	describe("404", () => {
-		it ("should return a 404 if page does not exist", done => {
-			request(app)
-				.get("/surely/this/pagewouldn/ot/exist")
-				.expect('Content-Type', /json/)
-				.expect(404)
-				.end(done);
-		});
-	});
+  describe("404", () => {
+    it("should return a 404 if page does not exist", async () => {
+      await request(app)
+        .get("/surely/this/pagewouldn/ot/exist")
+        .expect("Content-Type", /json/)
+        .expect(404);
+    });
+  });
 });
 
 describe("Misc", () => {
-	it ("should return a favicon", done => {
-		request(app)
-			.get("/favicon.ico")
-			.expect(200)
-			.end(done);
-	});
+  it("should return a favicon", async () => {
+    await request(app)
+      .get("/favicon.ico")
+      .expect(200);
+  });
 });
 
 describe("Utils", () => {
-	describe("Ping", () => {
-		it ("should pong", done => {
-			request(app)
-				.get("/ping")
-				.expect(200)
-				.expect("Content-Type", /json/)
-				.end((error, response) => {
-					if (error) return done(error);
-					assert.equal(response.body.result, "pong");
-					done();
-				});
-		});
-	});
+  describe("Ping", () => {
+    it("should pong", async () => {
+      const { body } = await request(app)
+        .get("/ping")
+        .expect(200)
+        .expect("Content-Type", /json/);
+      assert.equal(body.result, "pong");
+    });
+  });
 });
