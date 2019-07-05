ec119985adad5726f426209a5b03ac1a191e36c9
Christopher Blanchard <cablanchard@gmail.com>
2019-07-05 07:39:12 +0000

    chore(GSS): Update PCT

--- data/scripts/pcts.js
+++ data/scripts/pcts.js
@@ -14,14 +14,17 @@ const VALUE_OFFSET = 2;
 const transform = row => {
 	const code = row[CODE_OFFSET];
 	const value = row[VALUE_OFFSET];
-	if (code === "PCTCD") return []; // Escape if header
+	if (value === "PCTNM") return []; // Escape if header
 	return [code, value];
 };
 
+const parseOptions = { delimiter: "," };
+
 const configs = [
 	{
-		file: "PCT names and codes UK as at 12_16.txt",
-		transform
+		file: "PCT names and codes UK as at 04_19.csv",
+    transform,
+    parseOptions,
 	}
 ];
 
