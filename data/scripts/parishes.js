b57d7f1f651e9a6b3cd15003788bf187c24893a2
Christopher Blanchard <cablanchard@gmail.com>
2020-03-05 15:21:40 +0000

    chore(Parish): Update parish dataset

--- data/scripts/parishes.js
+++ data/scripts/parishes.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const {extract} = require("./index");
+const { extract } = require("./index");
 
 /**
  * @module DataParser/parishes
@@ -15,30 +15,30 @@ const CODE_OFFSET = 0;
 const VALUE_OFFSET = 1;
 
 const transform = row => {
-	const code = row[CODE_OFFSET];
-	const value = row[VALUE_OFFSET];
-	if (value === "PARNCP18NM") return []; // Escape if header
-	return [code, value];
+  const code = row[CODE_OFFSET];
+  const value = row[VALUE_OFFSET];
+  if (value === "PARNCP18NM") return []; // Escape if header
+  return [code, value];
 };
 
 const appendMissing = {
-	"E43000234": "Three Rivers, unparished area",
-	"E43000097": "Lancaster, unparished area",
-	"E43000135": "Waveney, unparished area",
-	"E43000245": "Swindon, unparished area"
+  E43000234: "Three Rivers, unparished area",
+  E43000097: "Lancaster, unparished area",
+  E43000135: "Waveney, unparished area",
+  E43000245: "Swindon, unparished area",
 };
 
 const parseOptions = { delimiter: "," };
 
 const configs = [
-	{
-		file: "Parish_NCP names and codes EW as at 12_18.csv",
+  {
+    file: "Parish_NCP names and codes EW as at 12_19.csv",
     transform,
     parseOptions,
-	},
+  },
 ];
 
-extract({ 
-	configs,
-	appendMissing
+extract({
+  configs,
+  appendMissing,
 });
