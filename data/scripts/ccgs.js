1ac901d604d7396c3babc343d26aa86fab7ad370
Christopher Blanchard <git@nf3.co>
2020-06-22 12:23:45 +0000

    chore(Data): Update CCGs
    England has its own file which has been updated to Apr 2020. Rest of UK
    stuck at April 2019

--- data/scripts/ccgs.js
+++ data/scripts/ccgs.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const {extract, isPseudoCode} = require("./index");
+const { extract, isPseudoCode } = require("./index");
 
 /**
  * @module DataParser/ccgs
@@ -12,34 +12,44 @@ const {extract, isPseudoCode} = require("./index");
  * - "CCG" suffix is dropped from values
  */
 
-
 const CODE_OFFSET = 0;
 const SECOND_CODE_OFFSET = 1;
 const VALUE_OFFSET = 2;
 
 const ccgRegex = /\sCCG$/;
 
-const transform = row => {
-	const code = row[CODE_OFFSET];
+const transform = (row) => {
+  const code = row[CODE_OFFSET];
   const second_code = row[SECOND_CODE_OFFSET];
-	const value = row[VALUE_OFFSET];
-	if (code === "CCG19CD") return []; // Escape if header
-	if (isPseudoCode(code)) return [];
-	return [code, {
-	  "ccg19cdh": second_code,
-    "name": value.replace(ccgRegex, "").trim()
-  }];
+  const value = row[VALUE_OFFSET];
+  if (code === "CCG19CD") return []; // Escape if header
+  if (isPseudoCode(code)) return [];
+  return [
+    code,
+    {
+      ccg19cdh: second_code,
+      name: value.replace(ccgRegex, "").trim(),
+    },
+  ];
 };
 
 const configs = [
-	{
-		file: "CCG names and codes UK as at 04_19.csv",
-		transform,
-		parseOptions: {
-			delimiter: ",",
-		},
-		encoding: "utf8",
-	}
+  {
+    file: "CCG names and codes UK as at 04_19.csv",
+    transform,
+    parseOptions: {
+      delimiter: ",",
+    },
+    encoding: "utf8",
+  },
+  {
+    file: "CCG names and codes EN as at 04_20.csv",
+    transform,
+    parseOptions: {
+      delimiter: ",",
+    },
+    encoding: "utf8",
+  },
 ];
 
 extract({ configs });
