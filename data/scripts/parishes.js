2d4af1322b4091b76b027384db74629163e6c4d3
Christopher Blanchard <git@nf3.co>
2022-08-24 21:48:22 +0000

    fix(Parish): Add missing parish data

--- data/scripts/parishes.js
+++ data/scripts/parishes.js
@@ -17,28 +17,18 @@ const VALUE_OFFSET = 1;
 const transform = (row) => {
   const code = row[CODE_OFFSET];
   const value = row[VALUE_OFFSET];
-  if (value === "PARNCP18NM") return []; // Escape if header
+  if (value === "PAR21CD") return []; // Escape if header
   return [code, value];
-};
-
-const appendMissing = {
-  E43000234: "Three Rivers, unparished area",
-  E43000097: "Lancaster, unparished area",
-  E43000135: "Waveney, unparished area",
-  E43000245: "Swindon, unparished area",
 };
 
 const parseOptions = { delimiter: "," };
 
 const configs = [
   {
-    file: "Parish_NCP names and codes EW as at 12_20.csv",
+    file: "PAR_DEC_2021_EW_NC.csv",
     transform,
     parseOptions,
   },
 ];
 
-extract({
-  configs,
-  appendMissing,
-});
+extract({ configs });
