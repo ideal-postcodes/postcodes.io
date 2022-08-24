47a93938091deb4c6056db7506739d210e28a8be
Christopher Blanchard <git@nf3.co>
2022-08-24 21:33:52 +0000

    fix(Wards): Update for missing wards

--- data/scripts/wards.js
+++ data/scripts/wards.js
@@ -14,17 +14,13 @@ const VALUE_OFFSET = 1;
 const transform = (row) => {
   const code = row[CODE_OFFSET];
   const value = row[VALUE_OFFSET];
-  if (code === "WD19CD") return []; // Escape if header
+  if (code === "WD22CD") return []; // Escape if header
   return [code, value];
-};
-
-const appendMissing = {
-  E05012396: "Darenth",
 };
 
 const configs = [
   {
-    file: "Ward names and codes UK as at 05_21 ONSPD.csv",
+    file: "Ward names and codes UK as at 05_22_onspd_v2.csv",
     transform,
     parseOptions: {
       delimiter: ",",
