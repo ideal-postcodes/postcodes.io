ef71ca8f8cb8807577bc4ecfab06567bb5654681
Christopher Blanchard <git@nf3.co>
2024-01-23 13:08:51 +0000

    Fix

--- src/bin/clear_test_db.ts
+++ src/bin/clear_test_db.ts
@@ -1,22 +1,11 @@
-import { parallel } from "async";
 import {
   Postcode,
-  District,
-  Parish,
-  County,
-  Ccg,
-  Constituency,
   ScottishConstituency,
   ScottishPostcode,
-  Nuts,
-  Ward,
   PoliceForceArea,
   Outcode,
   Place,
   TerminatedPostcode,
-  Ced,
-  Msoa,
-  Lsoa,
 } from "../app/models/index";
 
 const clear = async () => {
@@ -24,19 +13,9 @@ const clear = async () => {
   await TerminatedPostcode.destroyRelation();
   await Place.destroyRelation();
   await ScottishPostcode.destroyRelation();
-  await District.destroyRelation();
-  await Parish.destroyRelation();
-  await Nuts.destroyRelation();
-  await County.destroyRelation();
-  await Constituency.destroyRelation();
   await ScottishConstituency.destroyRelation();
-  await Ccg.destroyRelation();
-  await Ward.destroyRelation();
   await PoliceForceArea.destroyRelation();
   await Outcode.destroyRelation();
-  await Ced.destroyRelation();
-  await Lsoa.destroyRelation();
-  await Msoa.destroyRelation();
 };
 
 export const run = async (): Promise<void> => {
