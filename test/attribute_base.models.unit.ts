b334bd60c0d619997a1ec4cb2a8c6ac0d58a5c0f
Christopher Blanchard <git@nf3.co>
2024-02-16 16:14:34 +0000

    Fix

--- test/attribute_base.models.unit.ts
+++ test/attribute_base.models.unit.ts
@@ -1,30 +1,6 @@
 import { AttributeBaseSuite } from "./helper/index";
-import {
-  Ccg,
-  Ced,
-  County,
-  Constituency,
-  District,
-  ScottishConstituency,
-  Nuts,
-  Parish,
-  Ward,
-  Msoa,
-  Lsoa,
-  PoliceForceArea,
-} from "../src/app/models/index";
+import { ScottishConstituency } from "../src/app/models/index";
 
 const { rigCoreSpecs } = AttributeBaseSuite;
 
-rigCoreSpecs(Ccg);
-rigCoreSpecs(Ced);
-rigCoreSpecs(County);
-rigCoreSpecs(Constituency);
-rigCoreSpecs(District);
 rigCoreSpecs(ScottishConstituency);
-rigCoreSpecs(Nuts);
-rigCoreSpecs(Parish);
-rigCoreSpecs(Ward);
-rigCoreSpecs(Msoa);
-rigCoreSpecs(Lsoa);
-rigCoreSpecs(PoliceForceArea);
