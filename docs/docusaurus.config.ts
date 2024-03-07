a3a692cde590799785aab54b9f93b780b00f3b42
kerv14 <jkervinangeles@icloud.com>
2024-03-07 15:44:00 +0000

    update sidebar config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -34,7 +34,7 @@ const config: Config = {
       'classic',
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: require.resolve('./sidebars'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
