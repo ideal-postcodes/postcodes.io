f0d109429cc0b467d097bf80a266bf0e83f8c39c
kerv14 <jkervinangeles@icloud.com>
2024-03-11 22:52:26 +0000

    add endpoints

--- docs/src/components/Demos/index.tsx
+++ docs/src/components/Demos/index.tsx
@@ -12,21 +12,45 @@ export default function HomepageDemos(): JSX.Element {
         </div>
         <div>
           <PostcodeLookup
-            endpointTemplate="https://api.postcodes.io/postcodes/"
+            endpointTemplate="api.postcodes.io/postcodes/"
             linkEnd=""
             headingText="Lookup a postcode"
+            placeholder=":postcode"
           />
 
           <PostcodeLookup
-            endpointTemplate="https://api.postcodes.io/postcodes/"
+            endpointTemplate="api.postcodes.io/postcodes/"
             linkEnd="/validate"
             headingText="Validate a postcode"
+            placeholder=":postcode"
           />
 
           <PostcodeLookup
-            endpointTemplate="https://api.postcodes.io/postcodes/"
+            endpointTemplate="api.postcodes.io/postcodes/"
             linkEnd="/nearest"
             headingText="Nearest postcodes for postcode"
+            placeholder=":postcode"
+          />
+
+          <PostcodeLookup
+            endpointTemplate="api.postcodes.io/postcodes/"
+            linkEnd="/autocomplete"
+            headingText="Autocomplete a postcode partial"
+            placeholder=":postcode"
+          />
+
+          <PostcodeLookup
+            endpointTemplate="api.postcodes.io/postcodes?q="
+            linkEnd=""
+            headingText="Query for postcode"
+            placeholder=":postcode"
+          />
+
+          <PostcodeLookup
+            endpointTemplate="api.postcodes.io/terminated_postcodes/"
+            linkEnd=""
+            headingText="Lookup terminated postcode"
+            placeholder=":postcode"
           />
         </div>
       </div>
