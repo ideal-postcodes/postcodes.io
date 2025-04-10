a3fbd3466845fa4e7f9bf7b71ef1cd0b062b5e0d
Christopher Blanchard <git@nf3.co>
2025-04-10 10:08:20 +0000

    fix(NUTS/ITL): Update ITL for Feb 2025

--- data/scripts/nuts.js
+++ data/scripts/nuts.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const { extract } = require("./index");
 
 /**
@@ -22,15 +20,31 @@ const { extract } = require("./index");
  * }
  */
 
-const CODE_OFFSET = 2;
+// Example input from Feb 2025 release:
+//
+// ITL125CD,ITL125NM,ITL225CD,ITL225NM,ITL325CD,ITL325NM,LAU125CD,LAU125NM,LAD24CD,LAD24NM,,,,,,,
+// TLC,North East (England),TLC3,Tees Valley,TLC31,Hartlepool and Stockton-on-Tees,E06000001,Hartlepool,E06000001,Hartlepool,,,,,,,
+//
+// 0    , ITL125CD           , TLC
+// 1    , ITL125NM           , North East (England)
+// 2    , ITL225CD           , TLC3
+// 3    , ITL225NM           , Tees Valley
+// 4    , ITL325CD           , TLC31
+// 5    , ITL325NM           , Hartlepool and Stockton-on-Tees
+// 6    , LAU125CD           , E0600001
+// 7    , LAU125NM           , Hartlepool
+// 8    , LAD24CD            , E0600001
+// 9    , LAD24NM            , Hartlepool
+
+const CODE_OFFSET = 6;
 const NUTS_CODE_OFFSET = 4;
-const VALUE_OFFSET = 3;
+const VALUE_OFFSET = 7;
 
 const transform = (row) => {
   const code = row[CODE_OFFSET];
   const nutsCode = row[NUTS_CODE_OFFSET];
   const value = row[VALUE_OFFSET];
-  if (value === "NUTS318NM") return []; // Escape if header
+  if (value === "LAU125NM") return []; // Escape if header
   return [
     code,
     {
@@ -42,7 +56,7 @@ const transform = (row) => {
 
 const configs = [
   {
-    file: "LAD23_LAU121_ITL321_ITL221_ITL121_UK_LU.csv",
+    file: "ITL125_ITL225_ITL325_LAU125_LAD24_UK_LU.csv",
     transform,
     parseOptions: {
       delimiter: ",",
