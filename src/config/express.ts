45c5dcba2603d83465747cd568f44c233c962d0b
Christopher Blanchard <git@nf3.co>
2025-04-18 10:08:32 +0000

    Remove old views system
    Update body-parser

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,21 +1,10 @@
-import { Env, Config } from "./config";
-const env = (process.env.NODE_ENV as Env | undefined) || "development";
-import favicon from "serve-favicon";
-import bodyParser from "body-parser";
+import { Config } from "./config";
 import express from "express";
-import { join } from "path";
 import cors from "cors";
 
 export const expressConfig = (app: express.Express, config: Config) => {
   app.enable("trust proxy");
   app.disable("x-powered-by");
-  app.set("views", join(__dirname, "../../views"));
-  app.set("view engine", "ejs");
-
-  if (config.serveStaticAssets) {
-    app.use(favicon("public/favicon.ico"));
-    app.use(express.static(join(__dirname, "../../public")));
-  }
 
   const { httpHeaders } = config;
   if (httpHeaders) {
@@ -32,6 +21,6 @@ export const expressConfig = (app: express.Express, config: Config) => {
       allowedHeaders: "X-Requested-With, Content-Type, Accept, Origin",
     })
   );
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 };
